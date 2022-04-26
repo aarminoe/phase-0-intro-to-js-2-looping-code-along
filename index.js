f2911316ab65421f3186e6e1382b522b3760de7a
Aaron Noe <aarminoe@gmail.com>
2022-04-26 11:04:36 -0500

    commit tree 10299377bdc46c24c7df5dbe0aaf31e99619af1a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, surprise) {
+    let messages = []
+    for (let n = 0; n < name.length; n++) {
+        let mess = `Thank you, ${name[n]}, for the wonderful ${surprise} gift!`
+        messages.push(mess)
+    }
+    return messages
+}
+
+function countDown(num) {
+    for (let n = 0; n < num + 1; n++) {
+        console.log(n)
+    }
+}
